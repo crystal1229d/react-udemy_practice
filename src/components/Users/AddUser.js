ad154d3a0c24e94857c093dcc10319f074ea9841
crystal1229d <crystal1229d@naver.com>
2022-05-18 22:12:41 +0900

    add AddUser Component & add UI Components 1) add AddUser Component & style module 2) add UI/Button 3) add UI/Card 4) adapt Button & Card to AddUser Component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from '../UI/Button/Button'
+import Card from '../UI/Card/Card'
+
+import classes from './AddUser.module.css'
+
+const AddUser = (props) => {
+
+    const addUserHandler = (event) => {
+        event.preventDefault()
+    }
+
+    return (
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor='username'>Username</label>
+                <input id='username' type='text' />
+                <label htmlFor='userage'>Age</label>  
+                <input id='userage' type='number' />
+                <Button type='submit'>Add User</Button>
+            </form>
+        </Card>
+    )
+}
+
+export default AddUser
