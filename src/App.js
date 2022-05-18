1836b9e96464cb624f4260c53cdaf54cffd71758
crystal1229d <crystal1229d@naver.com>
2022-05-18 23:28:34 +0900

    add UsersList Component & fix (App & AddUser) 1) add UsersList Component & style module 2) adapt Card to UsersList 3) fix App for UsersList 4) fix AddUser for UsersList (add 'resetting & validation logic')

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
-
+import UsersList from './components/UsersList/UsersList';
 
 function App() {
+
+  const [UserList, setUsersList] = useState([])
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { id:Math.random().toString(), name: uName, age: uAge }]
+    })
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={UserList} />
     </div>
   );
 }
